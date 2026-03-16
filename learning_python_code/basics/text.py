f80d68e3fcbb0cc94ea81cdf3e4fa15fee0a1580
Vinay kumar <vinaythakur.coder@gmail.com>
2026-03-16 22:48:49 +0530

    check update

---
+++ learning_python_code/basics/text.py
@@ -0,0 +1,4 @@
+a = "thakur ji sahab"
+b = "thakur sahab"
+d = a + " " + b
+print(d)
---
+++ learning_python_code/py_libraries/Numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+array_01 = [1, 2, 3, 4, 5]
+array_02 = np.array(array_01)
+print(array_02)
